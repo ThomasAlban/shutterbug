c9cbba974d5c46383eb4f43c2250965c4e9339d7
ThomasAlban <thomasalbanmusic@gmail.com>
2023-12-22 14:10:39 +0000

    uncomment service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,8 +1,8 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference lib="webworker" />
 
-// const sw = self as unknown as ServiceWorkerGlobalScope;
+const sw = self as unknown as ServiceWorkerGlobalScope;
 
-// sw.addEventListener('push', () => {
-// 	sw.registration.showNotification('test', {});
-// });
+sw.addEventListener('push', () => {
+	sw.registration.showNotification('test', {});
+});
