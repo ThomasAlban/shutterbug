0b49f436a3806a89ce3732938c43cfc7f4b254d6
ThomasAlban <thomasalbanmusic@gmail.com>
2023-12-22 13:45:13 +0000

    another test

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,11 +1,13 @@
-// /// <reference types="@sveltejs/kit" />
-// /// <reference lib="webworker" />
+/// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
 
 // declare let self: ServiceWorkerGlobalScope;
 
+const sw = self as unknown as ServiceWorkerGlobalScope;
+
 // import * as sw from '$service-worker';
 
-// self.addEventListener('push', () => {
-// 	console.log('push');
-// 	self.registration.showNotification('test', {});
-// });
+sw.addEventListener('push', () => {
+	console.log('push');
+	sw.registration.showNotification('test', {});
+});
